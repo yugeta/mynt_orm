919f853dbcb6521c4d9481d833bd96e5072154ea
Yugeta.Koji <geta1972@gmail.com>
2026-01-30 14:03:29 +0900

    access_lists.phpのレスポンスデータに項目を追加

--- php/access_lists.php
+++ php/access_lists.php
@@ -9,15 +9,19 @@ class AccessLists{
 
   private function get_lists(){
     $lists = [];
-    $dir = __DIR__ . "/../data/";
+    $dir = realpath(__DIR__ . "/../data/");
     $files = scandir($dir);
     foreach($files as $file){
       if(in_array($file, [".", ".."])){continue;}
+      $json = file_get_contents($dir ."/". $file);
+      $data = json_decode($json, true) ?? [];
       $name = str_replace(".json", "", $file);
       $lists[] = [
         "dir"  => $dir,
         "file" => $file,
         "name" => $name,
+        "dbname" => $data["dbname"] ?? null,
+        "data" => $data,
       ];
     }
     return $lists;
